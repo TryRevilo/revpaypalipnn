9add050cd016ac0a04f2723960d6607a6cb041ec
root <root@localhost.localdomain>
2017-06-25 18:23:46 +0300

    Working Update

--- pay.php
+++ pay.php
@@ -5,16 +5,6 @@ use PayPal\Api\PaymentExecution;
 
 require 'app/start.php';
 
-if ( !isset( $_GET[ 'success' ], $_GET[ 'paymentId' ], $_GET[ 'payerID' ]) )
-{
-	die();
-}
-
-if ( (bool) $_GET[ 'success' ] === false )
-{
-	die();
-}
-
 $paymentId = $_GET[ 'paymentId' ];
 $payerId = $_GET[ 'payerID' ];
 
