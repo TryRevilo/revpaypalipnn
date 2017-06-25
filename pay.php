8915aa630aed4d6c8e3e548eec7f924b2d2922d9
root <root@localhost.localdomain>
2017-06-25 18:26:06 +0300

    Working Update

--- pay.php
+++ pay.php
@@ -5,18 +5,4 @@ use PayPal\Api\PaymentExecution;
 
 require 'app/start.php';
 
-$paymentId = $_GET[ 'paymentId' ];
-$payerId = $_GET[ 'payerID' ];
-
-$payment = Payment :: get( $paymentId, $paypal );
-
-$execute = new PaymentExecution();
-$execute -> setPayerId( $paymentId );
-
-try {
-	$result = $payment -> execute( $execute, $paypal );
-} catch ( Execution $e ) {
-	die( $e );
-}
-
 echo 'Payment Made. Thanks!';
