f3fe47296cd0a0d0a0719078ec70cc174f0e696d
root <root@localhost.localdomain>
2017-06-25 18:27:05 +0300

    Working Update

--- pay.php
+++ pay.php
@@ -5,4 +5,14 @@ use PayPal\Api\PaymentExecution;
 
 require 'app/start.php';
 
+if ( !isset( $_GET[ 'success' ], $_GET[ 'paymentId' ], $_GET[ 'payerID' ]) )
+{
+	die();
+}
+
+if ( (bool) $_GET[ 'success' ] === false )
+{
+	die();
+}
+
 echo 'Payment Made. Thanks!';
