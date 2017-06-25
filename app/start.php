c8c52fff3c9b06cba353db8666d2bbe39e8b9c5c
root <root@localhost.localdomain>
2017-06-25 16:55:42 +0300

    2

--- app/start.php
+++ app/start.php
@@ -7,7 +7,7 @@ define('SITE_URL', 'http://mmacadvocates.com/ipn');
 $paypal = new \PayPal\Rest\ApiContext (
 	new \PayPal\Auth\OAuthTokenCredential (
 		'AcVTJUpFWp-pqT3EqtlKJaj-5j8m5X-NrfEzkOB6NZGIH8lQpQ2IIvDTeuP4CphQLruHDKIYx20L1U2G',
-		'JW1WyPYRIpeJz63QiaOHptTqKtUlEkezXiaBIrZvLn3rsbECXUqgdik7MPfMF'
+		'EAmdP9zTdwen1ehb4p-JW1WyPYRIpeJz63QiaOHptTqKtUlEkezXiaBIrZvLn3rsbECXUqgdik7MPfMF'
 		)
 	);
 
