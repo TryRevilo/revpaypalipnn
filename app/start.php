433a37aad2f0f472608756a277c2bae8d8494742
root <root@localhost.localdomain>
2017-06-26 15:21:28 +0300

    Update App

--- app/start.php
+++ app/start.php
@@ -6,8 +6,8 @@ define('SITE_URL', 'https://revpaypalipnnoncomposer.herokuapp.com');
 
 $paypal = new \PayPal\Rest\ApiContext (
 	new \PayPal\Auth\OAuthTokenCredential (
-		'AcVTJUpFWp-pqT3EqtlKJaj-5j8m5X-NrfEzkOB6NZGIH8lQpQ2IIvDTeuP4CphQLruHDKIYx20L1U2G',
-		'EAmdP9zTdwen1ehb4p-JW1WyPYRIpeJz63QiaOHptTqKtUlEkezXiaBIrZvLn3rsbECXUqgdik7MPfMF'
+		'AayMa0gEpwAPLurJpGHRdTYOiMabSrZfm3Epf_Eu8BGtpjaizr6LizsgzMwvNRk6O_KBiNAKRPib9FT-',
+		'EN57E2IPhtRoFRs0QiVs5Z8IM4nGAonCiobqKo9mxr8OQdpOYAXq4lpIvxVdbySUcqYYb53tW2WNO5Ki'
 		)
 	);
 
